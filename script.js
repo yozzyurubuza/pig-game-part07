f0b753695f8b434931ac9d99f240b9f88d2728d8
yozzyurubuza <matt_ebarle15@yahoo.com>
2022-01-15 22:48:04 +0800

    Added starting conditions for the project

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+// Selecting elements
+//For classes
+const score0El = document.querySelector('#score--0');
+//For IDs, works fastter than querySelector
+const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+
+//Starting Conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
